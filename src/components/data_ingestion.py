252281e11735a31db4df3c6e1d0fba4a96281ffd
Pramod Kondur <pramod.kondur@gmail.com>
2024-10-17 20:03:45 +0530

    tweaks in data ingest

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -213,11 +213,11 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
-    #obj=DataIngestion()
-    #raw_data=obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    raw_data=obj.initiate_data_ingestion()
 
-    raw_data = ('artifacts/data.csv')
-    
+    #raw_data = ('artifacts/data.csv')
+
     data_transformation=DataTransformation()
     
     #data_transformation.initiate_data_transformation(train_data,test_data)
